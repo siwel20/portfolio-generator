9398e1ab4ce6a547ddb6c34afa31ff42ed2268f4
siwel20 <plthrock@gmail.com>
2020-06-24 20:35:06 -0600

    built HTML template and generated file

--- app.js
+++ app.js
@@ -1,23 +1,28 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 
+// const name = profileDataArgs[0];
+// const github = profileDataArgs[1];
+// line 2 and 3 do the same thing as line 7. Line 7 is just a simpler way to write it
+
+const [name, github] = profileDataArgs;
 
 
+// const printProfileData = profileDataArr => {
+//     for (let i = 0; i < profileDataArr.length; i += 1) {
+//         console.log(profileDataArr[i]);
+//     }
+//         console.log('================');
 
+            // for each does the same thing as the for loop above. 
+//         profileDataArr.forEach(profileItem => console.log(profileItem));
+// };
 
+// printProfileData(profileDataArgs);
 
-
-
-
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-        console.log(profileDataArr[i]);
-    }
-        console.log('================');
-
-        profileDataArr.forEach(profileItem => console.log(profileItem));
-};
-
-
-    
-
-printProfileData(profileDataArgs);
+  fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
+  
+    console.log('Portfolio complete! Check out index.html to see the output!');
+  });
