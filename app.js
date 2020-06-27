148faedfd2941189738983ba12a44f65cdd43cdc
siwel20 <plthrock@gmail.com>
2020-06-26 18:27:08 -0600

    added inquirer prompt function

--- app.js
+++ app.js
@@ -1,28 +1,42 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-
-// const name = profileDataArgs[0];
-// const github = profileDataArgs[1];
-// line 2 and 3 do the same thing as line 7. Line 7 is just a simpler way to write it
 
 const [name, github] = profileDataArgs;
 
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
 
-// const printProfileData = profileDataArr => {
-//     for (let i = 0; i < profileDataArr.length; i += 1) {
-//         console.log(profileDataArr[i]);
-//     }
-//         console.log('================');
 
-            // for each does the same thing as the for loop above. 
-//         profileDataArr.forEach(profileItem => console.log(profileItem));
-// };
 
-// printProfileData(profileDataArgs);
+// prior code for reference purposes. 
+  // const fs = require('fs');
+  // const generatePage = require('./src/page-template.js');
 
-  fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw err;
-  
-    console.log('Portfolio complete! Check out index.html to see the output!');
-  });
+
+  // const name = profileDataArgs[0];
+  // const github = profileDataArgs[1];
+
+  // const printProfileData = profileDataArr => {
+  //     for (let i = 0; i < profileDataArr.length; i += 1) {
+  //         console.log(profileDataArr[i]);
+  //     }
+  //         console.log('================');
+
+              // for each does the same thing as the for loop above. 
+  //         profileDataArr.forEach(profileItem => console.log(profileItem));
+  // };
+
+  // printProfileData(profileDataArgs);
+
+    // fs.writeFile('index.html', generatePage(name, github), err => {
+    //   if (err) throw err;
+    
+    //   console.log('Portfolio complete! Check out index.html to see the output!');
+    // });
